914c0494e41364825fc25c99d80a4294fb929793
Mahim <mahimmasrafi04@gmail.com>
2024-02-04 05:14:00 +0600

    Completed the site

---
+++ app.js
@@ -0,0 +1,6 @@
+const line1= document.getElementsByClassName("line1");
+const line2= document.getElementsByClassName("line2");
+const line3= document.getElementsByClassName("line3");
+function dropMenu() {
+    
+}
